4a39fcd2dba0f70c6ef5ea3322923054f8ceb1c6
Sharif Bhuiyan <106225926+phc-sbhuiyan@users.noreply.github.com>
2025-04-04 14:26:43 -0700

    Update Chat_Evaluation.py

--- pages/Chat_Evaluation.py
+++ pages/Chat_Evaluation.py
@@ -1,62 +1,51 @@
 import streamlit as st
 from utils import create_sidebar
-from trulens_eval import Tru
+from trulens_eval import TruChain, Feedback, Huggingface, Tru, feedback
+from pinecone import Pinecone
+from pinecone_plugins.assistant.models.chat import Message
+from langchain.chains import RetrievalQA
+from trulens.apps.custom import TruCustomApp
+from trulens.core import TruSession
+from trulens.apps.custom import instrument
+from trulens.dashboard import run_dashboard
 
 create_sidebar()
 
 tru = Tru()
-
-tru.run_dashboard()
-
-# import streamlit as st
-# from utils import create_sidebar
-# from trulens_eval import TruChain, Feedback, Huggingface, Tru, feedback
-# from pinecone import Pinecone
-# from pinecone_plugins.assistant.models.chat import Message
-# from langchain.chains import RetrievalQA
-# from trulens.apps.custom import TruCustomApp
-# from trulens.core import TruSession
-# from trulens.apps.custom import instrument
-# from trulens.dashboard import run_dashboard
-
-# create_sidebar()
-
-# tru = Tru()
 # session = TruSession()
 # session.reset_database()
 
-# # Initialize Huggingface-based feedback function collection class:
-# hugs = Huggingface()
+# Initialize Huggingface-based feedback function collection class:
+hugs = Huggingface()
 
-# # Define a language match feedback function using HuggingFace.
-# f_lang_match = Feedback(hugs.language_match).on_input_output()
-# # By default this will check language match on the main app input and main app
-# # output.
+# Define a language match feedback function using HuggingFace.
+f_lang_match = Feedback(hugs.language_match).on_input_output()
+# By default this will check language match on the main app input and main app
+# output.
 
-# # OpenAI as feedback provider
-# llm = feedback.OpenAI()
+# OpenAI as feedback provider
+llm = feedback.OpenAI()
 
-# # Question/answer relevance between overall question and answer.
-# qa_relevance = Feedback(llm.relevance).on_input_output()
+# Question/answer relevance between overall question and answer.
+qa_relevance = Feedback(llm.relevance).on_input_output()
 
-# api_key = st.secrets["PINECONE_API_KEY"]
-# pc = Pinecone(api_key=api_key)
-# assistant = pc.assistant.Assistant(
-#     assistant_name="pineapple-employee-assistant-bot", 
-# )
+api_key = st.secrets["PINECONE_API_KEY"]
+pc = Pinecone(api_key=api_key)
+assistant = pc.assistant.Assistant(
+    assistant_name="pineapple-employee-assistant-bot", 
+)
 
-# # wrap with TruLens
-# truchain = TruCustomApp(assistant,
-#     app_id='Chat_QA101',
-#     app_name="Chat_QA101 Assistant RAG",
-#     feedbacks=[f_lang_match, qa_relevance])
+# wrap with TruLens
+truchain = TruCustomApp(
+    assistant,
+    app_id='Chat_QA101_PHQ',
+    feedbacks=[f_lang_match, qa_relevance])
 
-# user_query = "Do you have any information about walk-in guests?"
-# msg = Message(role="user", content=user_query)
-# rag_assistant = assistant.chat(messages=[msg])
+user_query = "Do you have any information about walk-in guests?"
+msg = Message(role="user", content=user_query)
+with truchain as recording:
+  rag_assistant = assistant.chat(messages=[msg])
 
-# print(rag_assistant)
+tru.get_leaderboard(app_ids=["Chat_QA101_PHQ"])
 
-# session.get_leaderboard()
-
-# run_dashboard()
+tru.run_dashboard()
